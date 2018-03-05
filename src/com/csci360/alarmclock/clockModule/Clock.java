416eea07c983b5269de3912074ddaea05060e7d7
Ron Austin Taylor <ataylor4448@gmail.com>
2018-03-05 14:05:04 -0500

    main

---
+++ src/com/csci360/alarmclock/clockModule/Clock.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csci360.alarmclock.clockModule;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author Austin
+ */
+public class Clock {
+    public Clock(){
+    }
+    
+    public void updateTime(){
+    }
+
+    public static void main(String[] args){
+        Clock clk = new Clock();
+    }
+}
